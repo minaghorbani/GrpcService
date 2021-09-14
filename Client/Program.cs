53d463d5280cccd7dc8517d6cafdb6f952f7f4f3
Mina ghorbani <mina.gh.66@gmail.com>
2021-09-14 09:30:39 +0430

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello gRPC !\n");
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Mina" });
+
+            Console.WriteLine(reply.Message);
+        }
+    }
+}
